71acfdedc44fe5997a86810c834788ea8fc50454
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2023-02-04 11:57:31 +0530

    managing token

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,4 +1,5 @@
-import { useRef, useState } from 'react';
+import { useContext, useRef, useState } from 'react';
+import AuthContext from '../../store/auth-context';
 
 import classes from './AuthForm.module.css';
 
@@ -7,6 +8,7 @@ const AuthForm = () => {
   const passwordInputRef=useRef('');
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading,setIsLoading]=useState(false);
+  const authCtx=useContext(AuthContext);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -48,7 +50,7 @@ const AuthForm = () => {
         })
       }
       }).then((data)=>{
-        console.log(data)
+        authCtx.login(data.idToken)
       })
       .catch((err)=>{
         alert(err)
